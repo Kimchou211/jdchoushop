ce8cff2125a6a7e770568cd6a029eae10725edeb
Kimchou211 <krenkinchou@gmail.com>
2026-03-08 19:33:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ async function initDB() {
   try {
     db = await mysql.createPool({
       host             : process.env.DB_HOST || 'localhost',
+      port             : process.env.DB_PORT || 3306,
       user             : process.env.DB_USER || 'root',
       password         : process.env.DB_PASS || '',
       database         : process.env.DB_NAME || 'nyka_shop',
